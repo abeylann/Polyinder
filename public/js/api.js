ebbcc43c2b3f1e23a00af2bc493e80bac5cc5289
Jens Elstner <jens.elstner@technicallyfeasible.com>
2015-04-25 21:11:41 +0100

    generate fixed session id

--- public/js/api.js
+++ public/js/api.js
@@ -3,6 +3,17 @@ define(['jquery'], function($) {
     var apiUrl = 'http://localhost:4567';
 
     var api = {
+        getSession: function() {
+            // get current user session
+            var session = localStorage.getItem('user_session');
+            if (!session) {
+                session = new Date().getTime() + '' + (Math.random() * 1000).toFixed(0);
+                localStorage.setItem('user_session', session);
+            }
+            console.log('current session:', session);
+            return session;
+        },
+
         getPolicies: function(cb) {
             if (!cb) return;
             $.ajax({
@@ -32,7 +43,10 @@ define(['jquery'], function($) {
                 url: apiUrl + '/policy/' + id + '/vote',
                 method: 'POST',
                 contentType: 'application/json',
-                data: JSON.stringify({vote: (yes ? 'yes' : 'no')}),
+                data: JSON.stringify({
+                    session: api.getSession(),
+                    vote: (yes ? 'yes' : 'no')
+                }),
                 complete: function(res) {
                     if (res.status !== 200)
                         return;

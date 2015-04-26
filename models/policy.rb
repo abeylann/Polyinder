edd7994ac74b955ba8b6509363e441bc051e3552
Michael Wawra <michael@wawra.co.uk>
2015-04-26 08:30:05 +0100

    adding share of votes

--- models/policy.rb
+++ models/policy.rb
@@ -5,12 +5,14 @@ class Policy < ActiveRecord::Base
   def present
     puts impacts
     i = impacts.collect { |i| i.present} unless impacts == nil
+    share = (pledged.to_f/votes.count).round(2) if votes.count > 0 && pledged != 0
     {
       id: id,
       title: name,
       pledged: pledged,
       yes: votes.where(yes: true).count,
       no: votes.where(yes: false).count,
+      share: share,
       picture: image_url,
       impact: i
     }
